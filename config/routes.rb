585b92aefac7af6681bbfe04d24189b4d14825e9
Eduardo Arenas <adbeel92@gmail.com>
2020-11-19 00:51:45 -0600

    Add root path

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
 
-  root 'active_admin/devise/sessions#new'
+  root 'admin/dashboard#index'
 end
